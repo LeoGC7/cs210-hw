be64e8c2525c17ff67da3071e06057234f0f4942
LeoGC7 <leonardo.gameiro.correa@gmail.com>
2024-04-30 15:38:44 -0300

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your score? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        if (gradeNumber >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        
+        else if (gradeNumber >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+
+        else if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+
+        else if (gradeNumber >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }
