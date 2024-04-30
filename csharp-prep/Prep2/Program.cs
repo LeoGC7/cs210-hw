f18b070b197890ca85955624bf272bec0e9f0e0b
LeoGC7 <leonardo.gameiro.correa@gmail.com>
2024-04-30 15:41:02 -0300

    Finished Prep2 You passed or failed message added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -11,25 +11,30 @@ class Program
         if (gradeNumber >= 90)
         {
             Console.WriteLine("Your grade is A");
+            Console.WriteLine("You passed in the class! Congratulations!");
         }
         
         else if (gradeNumber >= 80)
         {
             Console.WriteLine("Your grade is B");
+            Console.WriteLine("You passed in the class! Congratulations!");
         }
 
         else if (gradeNumber >= 70)
         {
             Console.WriteLine("Your grade is C");
+            Console.WriteLine("You passed in the class! Congratulations!");
         }
 
         else if (gradeNumber >= 60)
         {
             Console.WriteLine("Your grade is D");
+            Console.WriteLine("Sorry, you failed in the class. Try harder next time!");
         }
         else
         {
             Console.WriteLine("Your grade is F");
+            Console.WriteLine("Sorry, you failed in the class. Try harder next time!");
         }
     }
 }
