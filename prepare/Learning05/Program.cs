2b7365d43b1fd44b96a94e8dbeb57cadc471a066
LeoGC7 <leonardo.gameiro.correa@gmail.com>
2024-06-05 14:11:35 -0300

    Learning05 finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 10, 4);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Red", 5);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has the area of {area}");
+        }
     }
 }
